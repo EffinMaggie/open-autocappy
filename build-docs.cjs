92143c482542fa7c53a59f328940a7fe5fcd0df3
Maggie Danger <magnus@ef.gy>
2021-09-21 23:08:55 +0100

    docs: try a different approach to building doxygen docs

--- build-docs.cjs
+++ build-docs.cjs
@@ -1,16 +1,5 @@
 var doxygen = require('doxygen');
 
 doxygen.downloadVersion().then(function (data) {
-  doxygen.createConfig({
-    OUTPUT_DIRECTORY: 'documentation',
-    INPUT: './',
-    RECURSIVE: 'YES',
-    GENERATE_HTML: 'YES',
-    HTML_OUTPUT: 'doxygen',
-    FILE_PATTERNS: ['*.ts', '*.md'],
-    EXTENSION_MAPPING: 'ts=Javascript',
-    GENERATE_LATEX: 'NO',
-    EXCLUDE_PATTERNS: ['*/node_modules/*']
-  }, './doxyfile');
   doxygen.run('./doxyfile');
 });
