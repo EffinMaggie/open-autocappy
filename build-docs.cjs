2f4b8912fbcfb038640ba2666bb13b5e6fe7aeac
Maggie Danger <magnus@ef.gy>
2021-09-21 21:26:02 +0100

    chore: update linting and formatting

--- build-docs.cjs
+++ build-docs.cjs
@@ -5,6 +5,8 @@ doxygen.downloadVersion().then(function (data) {
     OUTPUT_DIRECTORY: 'documentation',
     INPUT: './',
     RECURSIVE: 'YES',
+    GENERATE_HTML: 'YES',
+    HTML_OUTPUT: 'doxygen',
     FILE_PATTERNS: ['*.ts', '*.md'],
     EXTENSION_MAPPING: 'ts=Javascript',
     GENERATE_LATEX: 'NO',
