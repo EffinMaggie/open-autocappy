2ae48e622e2c9572c80488dd2715e3c23e678542
Maggie Danger <magnus@ef.gy>
2021-09-21 18:18:17 +0100

    fix: stop committing JS files
    HUSKY=0
    
    *.js are generated by TypeScript, so there's no need for them to be
    committed.
    
    *.cjs are exempt from this, as they are CommonJS and written by hand.
    
    Neat how clean that cut is!

--- test.js
+++
@@ -1,6 +0,0 @@
-import { testModule } from './test/run.js';
-import * as qualified from './test/qualified.js';
-import * as dated from './test/dated.js';
-export var testsOK = true;
-testsOK && (testsOK = testModule(qualified));
-testsOK && (testsOK = testModule(dated));
