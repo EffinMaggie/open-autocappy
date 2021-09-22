d0f64eaab94b9151e042887be34cc32e69403d3a
Maggie Danger <magnus@ef.gy>
2021-09-23 00:27:11 +0100

    feat: lint.ts will now print diffs

--- lint.ts
+++ lint.ts
@@ -3,6 +3,15 @@
 import { readFile } from 'fs';
 import { default as prettier } from 'prettier';
 import { format } from './lint.config.js';
+import { diffLines } from 'diff';
+import esMain from 'es-main';
+import chalk from 'chalk';
+
+interface chunk {
+  added?: boolean;
+  removed?: boolean;
+  value: any;
+}
 
 interface common {
   file: string;
@@ -11,10 +20,15 @@ interface common {
 
 interface update extends common {
   pass: true;
+  clean: true | null;
   source: string;
   pretty: string;
-  check: boolean;
-  updated: boolean;
+}
+
+interface diff extends common {
+  pass: true;
+  clean: false;
+  changes: Array<chunk>;
 }
 
 interface reason extends common {
@@ -22,7 +36,7 @@ interface reason extends common {
   reason: object;
 }
 
-export type result = update | reason;
+export type result = update | diff | reason;
 
 export function prettify(file) {
   return new Promise((resolve, reject) => {
@@ -34,14 +48,14 @@ export function prettify(file) {
       prettier.resolveConfig(file).then(
         (options: object): void => {
           options['filepath'] = file;
+          var pretty = prettier.format(data, options);
 
           resolve({
             file: file,
             pass: true,
+            clean: data === pretty ? true : undefined,
             source: data,
-            pretty: prettier.format(data, options),
-            check: prettier.check(data, options),
-            updated: false,
+            pretty: pretty,
           });
         },
         (err: object): void => {
@@ -56,30 +70,46 @@ export function prettify(file) {
   });
 }
 
+function diff(update: update): diff {
+  let d = diffLines(update.source, update.pretty);
+
+  return {
+    file: update.file,
+    pass: true,
+    clean: false,
+    changes: d,
+  };
+}
+
+function formatFiles(files: Array<string>): Array<Promise<result>> {
+  let ps = [];
+
+  for (const file of files) {
+    ps.push(
+      prettify(file).then(
+        (u: update): update | diff => {
+          if (u.clean) {
+            return u;
+          }
+          return diff(u);
+        },
+        (reason: reason): reason => {
+          return {
+            file: file,
+            pass: false,
+            reason: reason.reason,
+          };
+        }
+      )
+    );
+  }
+  return ps;
+}
+
 export function reformat(write: boolean): Promise<Array<result>> {
   return new Promise((resolve, reject): void => {
-    format.then((files) => {
-      let ps = [];
-
-      for (const file of files) {
-        ps.push(
-          prettify(file).then(
-            (processed: update): update => {
-              if (!processed.check) {
-                processed.file = file;
-              }
-              return processed;
-            },
-            (reason: reason): reason => {
-              return {
-                file: file,
-                pass: false,
-                reason: reason.reason,
-              };
-            }
-          )
-        );
-      }
+    format.then((files: Array<string>) => {
+      let ps = formatFiles(files);
 
       Promise.all(ps).then(
         (results: Array<result>): void => {
@@ -88,21 +118,58 @@ export function reformat(write: boolean): Promise<Array<result>> {
         (reason: reason): void => {
           console.warn(reason);
 
-          reject({
-            reason: reason,
-          });
+          reject(reason);
         }
       );
     });
   });
 }
 
-reformat(false).then((result: Array<result>) => {
-  let results = result.filter((e: result) => {
-    return !e.pass || (!e.check && !e.updated);
+function show(dirty: Array<result>, output: (s: boolean, r: string) => void): void {
+  for (const result of dirty) {
+    if (result.pass && result.clean === false) {
+      output(
+        true,
+        chalk.magenta.bold('LINT') +
+          ' ' +
+          chalk.dim(result.file) +
+          ': formatting different to "' +
+          chalk.underline('prettier') +
+          '" formatting; run "' +
+          chalk.underline('npm run fix') +
+          '" before commit'
+      );
+
+      for (const chunk of result.changes) {
+        let val = chunk.value.replace(/^\s+|\s$/g, '');
+        if (val.length > 0) {
+          if (chunk.added) {
+            output(false, chalk.blue.underline(val));
+          } else if (chunk.removed) {
+            output(false, chalk.dim.strikethrough(val));
+          } else {
+            output(false, '[ ... ]');
+          }
+        }
+      }
+    }
+  }
+}
+
+if (esMain(import.meta)) {
+  reformat(false).then((result: Array<result>) => {
+    let dirty = result.filter((e: result): boolean => {
+      return !e.pass || !e.clean;
+    });
+
+    show(dirty, (warning: boolean, message: string): void => {
+      if (warning) {
+        console.warn(message);
+      } else {
+        console.log(message);
+      }
+    });
+
+    process.exit(dirty.length);
   });
-
-  console.warn(results);
-
-  process.exit(results.length);
-});
+}
