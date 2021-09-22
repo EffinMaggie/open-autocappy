ae233d3d9950a919b0a74e66b5eaf96add39d654
Maggie Danger <magnus@ef.gy>
2021-09-22 11:47:49 +0100

    fix: don't hide the configs for prettier
    I don't think this should be a hidden file, so instead let's make it a normal CJS config.

---
+++ prettier.config.cjs
@@ -0,0 +1,19 @@
+/** @format */
+
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 103,
+  semi: true,
+  singleQuote: true,
+  useTabs: false,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: true,
+  proseWrap: 'always',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+};
