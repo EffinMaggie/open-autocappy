f50854cda6a9803237a779a86235bb6708d5b504
Lumpy Space Princess <buttercup.rules.xxx@lumpy.space>
2021-09-25 11:04:04 +0000

    chore: freshen up documentation 3f05b41fa789c595f143f7cdab20c7fd43ee3586

--- search/all_1.js
+++ search/all_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['carrier_2',['carrier',['../classcarrier.html',1,'']]],
   ['changelog_3',['CHANGELOG',['../md_CHANGELOG.html',1,'']]],
-  ['constructor_4',['constructor',['../classOuterHull_3_01Type_01extends_01Qualified_01_4.html#ae23923dad3776fede69747c46691f707',1,'OuterHull&lt; Type extends Qualified &gt;']]]
+  ['constructor_4',['constructor',['../classTranscript.html#a9dabb983659cc4a6ae83052cf4621da0',1,'Transcript::constructor()'],['../classOuterHull_3_01Type_01extends_01Qualified_01_4.html#a309d409873bdec5a81e364a86dfe6cec',1,'OuterHull&lt; Type extends Qualified &gt;::constructor()']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['constructor_23',['constructor',['../classOuterHull_3_01Type_01extends_01Qualified_01_4.html#ae23923dad3776fede69747c46691f707',1,'OuterHull&lt; Type extends Qualified &gt;']]]
+  ['constructor_23',['constructor',['../classTranscript.html#a9dabb983659cc4a6ae83052cf4621da0',1,'Transcript::constructor()'],['../classOuterHull_3_01Type_01extends_01Qualified_01_4.html#a309d409873bdec5a81e364a86dfe6cec',1,'OuterHull&lt; Type extends Qualified &gt;::constructor()']]]
 ];
