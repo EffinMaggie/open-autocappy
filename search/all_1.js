1ab141dcab827f40a0d8ac5874d2573467fe9a3d
Lumpy Space Princess <buttercup.rules.xxx@lumpy.space>
2021-09-29 12:28:26 +0000

    chore: freshen up documentation 42c609da7226349886a3bf6639b4d0450800a94e

--- search/all_1.js
+++ search/all_1.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['carrier_2',['carrier',['../classcarrier.html',1,'']]],
   ['changelog_3',['CHANGELOG',['../md_CHANGELOG.html',1,'']]],
-  ['constructor_4',['constructor',['../classTranscript.html#a9dabb983659cc4a6ae83052cf4621da0',1,'Transcript::constructor()'],['../classOuterHull_3_01Type_01extends_01PartialOrder_01_4.html#aca30ae7ed7a5753383fa0f7286a0710e',1,'OuterHull&lt; Type extends PartialOrder &gt;::constructor()']]]
+  ['constructor_4',['constructor',['../classOuterHull_3_01Type_01extends_01PartialOrder_01_4.html#aca30ae7ed7a5753383fa0f7286a0710e',1,'OuterHull&lt; Type extends PartialOrder &gt;']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['constructor_23',['constructor',['../classTranscript.html#a9dabb983659cc4a6ae83052cf4621da0',1,'Transcript::constructor()'],['../classOuterHull_3_01Type_01extends_01PartialOrder_01_4.html#aca30ae7ed7a5753383fa0f7286a0710e',1,'OuterHull&lt; Type extends PartialOrder &gt;::constructor()']]]
+  ['constructor_23',['constructor',['../classOuterHull_3_01Type_01extends_01PartialOrder_01_4.html#aca30ae7ed7a5753383fa0f7286a0710e',1,'OuterHull&lt; Type extends PartialOrder &gt;']]]
 ];
